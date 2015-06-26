d4fc2c97b4254161ca3108e978e99b5c2e6d3b26
hex <hex@hex.com>
2015-06-26 16:44:50 +0800

    Code tesed on the robot, the position limit should be dealt with in the following revisions

--- Client.cpp
+++ Client.cpp
@@ -36,10 +36,13 @@ int OnControlCommandNeeded(Aris::Core::MSG &msg)
     cout << "10.Backward"    << endl << "11.Fast_Forward"  << endl << "12.Fast_Backward" << endl;
     cout << "13.Legup"       << endl << "14.Turnleft"      << endl << "15.TurnRight"     << endl;
     cout << "16.Online Gait" << endl << "17.Online start"  << endl << "18.Online end"    << endl;
+    cout << "19.No force"    << endl << "20.Force 1"       << endl << "21.Force 2"       << endl;
+    cout << "22.Force 3"     << endl << "23.Force 4"       << endl << "24.Force 5"       << endl;
+    cout << "25.Force 6"     << endl << "26.Force 7"       << endl << "27.Force 8"       << endl;
 
     cout<<"Please enter your command: ";
     cin>>cmd;
-    while( cmd < 1 || cmd > 18 )
+    while( cmd < 1 || cmd > 27 )
     {
         cout<<"Not valid command ID,enter again : ";
         cin>>cmd;
